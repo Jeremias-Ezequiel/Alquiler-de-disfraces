1199121f8710f8ddb0473b4d0a4a796af619bb01
Omonte Jeremias <jeremias150817@gmail.com>
2026-01-06 11:13:03 -0300

    update: Table and column names were changed to English

--- database.sql
+++ database.sql
@@ -4,60 +4,59 @@ CREATE DATABASE IF NOT EXISTS OmonteJeremiasFinal;
 -- Usar BD
 USE OmonteJeremiasFinal;
 
--- Estado Disfraz
-CREATE TABLE IF NOT EXISTS estadodisfraz(
+-- Status Tables
+
+CREATE TABLE IF NOT EXISTS costumeStatus(
     id TINYINT(4),
-    descripcion VARCHAR(23),
+    description VARCHAR(23),
     PRIMARY KEY(id)
 ); 
 
--- Roles
 CREATE TABLE IF NOT EXISTS roles(
-    rol TINYINT(4),
-    descripcion VARCHAR(30),
+    id TINYINT(4),
+    description VARCHAR(30),
     PRIMARY KEY(rol)
 );
 
--- Estado Alquiler
-CREATE TABLE IF NOT EXISTS estadoalquiler(
+CREATE TABLE IF NOT EXISTS rentalStatus(
     id TINYINT(4),
-    descripcion VARCHAR(30),
+    description VARCHAR(30),
     PRIMARY KEY(id)
 );
 
--- Disfraz
-CREATE TABLE IF NOT EXISTS disfraz (
-    nroDisfraz INT(11) AUTO_INCREMENT NOT NULl,
-    descripcion VARCHAR(30),
-    talle TINYINT(4),
-    estado TINYINT(4),
+-- Main Tables
+
+CREATE TABLE IF NOT EXISTS costume (
+    id INT(11) AUTO_INCREMENT NOT NULl,
+    description VARCHAR(30),
+    size TINYINT(4),
+    status_id TINYINT(4),
     PRIMARY KEY(nroDisfraz),
-    FOREIGN KEY (estado) REFERENCES estadodisfraz(id)
+    FOREIGN KEY (estado_id) REFERENCES costumeStatus(id)
 );
 
--- Usuario
-CREATE TABLE IF NOT EXISTS usuario(
-    idUsuario INT(11) AUTO_INCREMENT NOT NULL,
-    nombre VARCHAR(30),
-    apellido VARCHAR(30),
-    nombreUsuario VARCHAR(30) NOT NULL,
-    contra VARCHAR(20),
-    direccion VARCHAR(40),
-    tel BIGINT(20),
-    rol TINYINT(4),
-    PRIMARY KEY(idUsuario),
-    FOREIGN KEY (rol) REFERENCES roles(rol) 
+CREATE TABLE IF NOT EXISTS user(
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    lastname VARCHAR(30),
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(255),
+    address VARCHAR(40),
+    phone BIGINT(20),
+    role_id TINYINT(4),
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) 
 );
 
 -- Alquileres
-CREATE TABLE IF NOT EXISTS alquileres(
-    nro INT(11) AUTO_INCREMENT NOT NULL,
-    usuario INT(11),
-    disfraz INT(11),
-    fecha DATE DEFAULT (CURRENT_DATE),
-    estado TINYINT(4),
-    PRIMARY KEY(nro),
-    FOREIGN KEY (usuario) REFERENCES usuario(idUsuario),
-    FOREIGN KEY (disfraz) REFERENCES disfraz(nroDisfraz),
-    FOREIGN KEY (estado) REFERENCES estadoalquiler(id)
+CREATE TABLE IF NOT EXISTS rental(
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    user_id INT(11),
+    costume_id INT(11),
+    rent_date DATE DEFAULT (CURRENT_DATE),
+    status_id TINYINT(4),
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (costume_id) REFERENCES costume(id),
+    FOREIGN KEY (status_id) REFERENCES rentalStatus(id)
 );
